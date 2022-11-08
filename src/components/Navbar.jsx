f7aca53e4dac47f3508d4c70372457f63babf50d
Andrez Ramirez <andrezzz2312@gmail.com>
2022-11-08 18:00:39 -0500

    1 fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import '../styles/components/Navbar.sass'
 
 const Navbar = () => {
-  return (
-    <nav className='Navbar'>
-      <a className='Navbar__Button' href="">ABOUT</a>
-      <a className='Navbar__Button' href="">FEEDBACK</a>
-      <a className='Navbar__Button' href="">CONTACT</a>
-      <button className='Navbar__Button'>
-        <img className='Navbar__SearchIcon' src='./bluemoon/assets/lupa.svg'/>
-      </button>
-      <a className='Navbar__Button--Special' href="">CONNECT WALLET</a>
-    </nav>
-  )
+	return (
+		<nav className='Navbar'>
+			<a className='Navbar__Button'>ABOUT</a>
+			<a className='Navbar__Button'>FEEDBACK</a>
+			<a className='Navbar__Button'>CONTACT</a>
+			<button className='Navbar__Button'>
+				<img className='Navbar__SearchIcon' src='./bluemoon/assets/lupa.svg' />
+			</button>
+			<a className='Navbar__Button--Special'>CONNECT WALLET</a>
+		</nav>
+	)
 }
 
 export default Navbar
