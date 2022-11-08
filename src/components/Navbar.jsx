d639717987df89a9831104fb13d4df7572537d2f
Andrez Ramirez <andrezzz2312@gmail.com>
2022-11-08 18:03:17 -0500

    asd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,21 @@ import '../styles/components/Navbar.sass'
 const Navbar = () => {
 	return (
 		<nav className='Navbar'>
-			<a className='Navbar__Button'>ABOUT</a>
-			<a className='Navbar__Button'>FEEDBACK</a>
-			<a className='Navbar__Button'>CONTACT</a>
+			<a className='Navbar__Button' href='/'>
+				ABOUT
+			</a>
+			<a className='Navbar__Button' href='/'>
+				FEEDBACK
+			</a>
+			<a className='Navbar__Button' href='/'>
+				CONTACT
+			</a>
 			<button className='Navbar__Button'>
 				<img className='Navbar__SearchIcon' src='./bluemoon/assets/lupa.svg' />
 			</button>
-			<a className='Navbar__Button--Special'>CONNECT WALLET</a>
+			<a className='Navbar__Button--Special' href='/'>
+				CONNECT WALLET
+			</a>
 		</nav>
 	)
 }
